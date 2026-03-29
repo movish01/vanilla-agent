10631c3872a95ebdcd06da01bbfa0d56f37f461e
I588095 <movish.verma@sap.com>
2026-03-29 15:14:45 +0530

    added structured output

--- agent/agent.py
+++ agent/agent.py
@@ -1,5 +1,6 @@
 from typing import Any
 from shared.llm import LocalLLM
+from shared.utils import extract_json_from_text
 
 class Agent:
     def __init__(self, model_path: str):
@@ -30,3 +31,36 @@ class Agent:
         response = response.replace('<SYSTEM>', '').replace('</SYSTEM>', '').strip()
         response = response.replace('<USER>', '').replace('</USER>', '').strip()
         return response
+    
+    def generate_structured(self, user_input: str, schema: str) -> dict | None:
+        """Generate structure JSON outputwith vaidations
+
+        Args:
+            user_input (str): _description_
+            schema (str): _description_
+
+        Returns:
+            dict | None: _description_
+        """
+        
+        prompt = f"""{self.system_prompt}
+            CRITICAL INSTRUCTIONS:
+            1. Respond with ONLY JSON
+            2. No explanations, no markdown, no extrac test before or after the JSON response
+            3. Start your response with {{ and end with }}
+            
+            Schema you must follow:
+            {schema}
+            
+            User request: {user_input}
+            
+            Reponse (JSON only):"""
+        
+        for attempt in range(3):
+            response = self.llm.generate(prompt, temperature=0.0)
+            parsed = extract_json_from_text(response) 
+            
+            if parsed is not None:
+                return parsed
+        
+        return None
--- main.py
+++ main.py
@@ -1,5 +1,18 @@
 from agent.agent import Agent
 
 agent = Agent("models/Meta-Llama-3.1-8B-Instruct-Q4_K_M.gguf")
-response = agent.generate_with_role("What is an AI agent?")
-print(response)
+
+# Generation with System Prompt
+# response = agent.generate_with_role("What is an AI agent?")
+
+# Structured Generation with JSON output
+schema = '''
+{
+    "explanation" : "string",
+    "difficulty" : "beginner" | "intermediate" | "advanced"
+}
+'''
+
+result = agent.generate_structured("Explain Computer Vision", schema)
+
+print(result)
