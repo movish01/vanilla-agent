98ddbddbf7018c7546f74b42a9084a6770c0ecb6
I588095 <movish.verma@sap.com>
2026-03-29 12:40:02 +0530

    added system prompt

--- agent/agent.py
+++ agent/agent.py
@@ -1,13 +1,32 @@
 from typing import Any
-# from agent.agent import Agent
 from shared.llm import LocalLLM
 
 class Agent:
     def __init__(self, model_path: str):
         self.llm = LocalLLM(model_path)
+        self.system_prompt = (
+            "You are a helpful and precise assistant for answering questions.",
+            "You explain concepts simply and clearly, and avoid unnecessary details.",
+            "If you don't know the answer, say you don't know, don't try to make up an answer."
+        )
 
     def simple_generate(self, user_input: str) -> str:
         """
         Generate a response based on the user input.
         """
         return self.llm.generate(user_input)
+    
+    def generate_with_role(self, user_input: str) -> str:
+        """
+        Generate a response with system prompt.
+        """
+        prompt = f"""{self.system_prompt}
+        User: {user_input}
+        Assistant:"""
+        
+        response = self.llm.generate(prompt)
+        
+        #Cleaning response
+        response = response.replace('<SYSTEM>', '').replace('</SYSTEM>', '').strip()
+        response = response.replace('<USER>', '').replace('</USER>', '').strip()
+        return response
--- main.py
+++ main.py
@@ -1,5 +1,5 @@
 from agent.agent import Agent
 
 agent = Agent("models/Meta-Llama-3.1-8B-Instruct-Q4_K_M.gguf")
-response = agent.simple_generate("What is an AI agent?")
+response = agent.generate_with_role("What is an AI agent?")
 print(response)
